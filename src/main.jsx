581765415c5bea3a5b1433111768b65d60ffac37
Eine0322 <eine113589@gmail.com>
2025-08-24 09:50:18 +0900

    [style] css 전체 안정화

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './index.css'
-import './styles/font.css'
+import './styles/reset.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
