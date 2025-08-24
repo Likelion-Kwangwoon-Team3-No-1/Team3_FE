c7d4abe25cb2063134418f3856e3c32cc9678437
Eine0322 <eine113589@gmail.com>
2025-08-24 15:13:21 +0900

    [feat] 온보딩 구현

--- src/api/client.js
+++ src/api/client.js
@@ -26,12 +26,12 @@ instance.interceptors.request.use(
 
     const token = localStorage.getItem('ACCESS_TOKEN')
     if (token) {
-      /*배포시 변경 config.headers.Authorization = `Bearer ${token}`*/
-      /* 자영업자 토큰
-      config.headers.Authorization = `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJIb3N0QWRtaW4iLCJyb2xlIjoiUk9MRV9IT1NUIiwiaWF0IjoxNzU1OTkyODkzLCJleHAiOjE3NTU5OTY0OTN9.Gs5-i3PwUyWGQF7mgDlS1UN5lDEWQ83HrmLhu9mpg3o`
-*/
-      /* 대학생 토큰*/
-      config.headers.Authorization = `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJNYXRlQWRtaW4iLCJyb2xlIjoiUk9MRV9NQVRFIiwiaWF0IjoxNzU1OTkyODY0LCJleHAiOjE3NTU5OTY0NjR9.SHfXK_1BLwNQuFJGQ8yGbDzTi0jcsK-T8LjPmqFIDrc`
+      /*배포시 변경 */
+      //config.headers.Authorization = `Bearer ${token}`
+      /* 자영업자 토큰*/
+      config.headers.Authorization = `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJIb3N0QWRtaW4iLCJyb2xlIjoiUk9MRV9IT1NUIiwiaWF0IjoxNzU2MDEzNzAwLCJleHAiOjE3NTYwMTczMDB9.eeBtQF9Pk4tNgFRiS2qGZnmsAo7P8C12je7jt3MVuJo`
+      /* 대학생 토큰  */
+      //config.headers.Authorization = `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJNYXRlQWRtaW4iLCJyb2xlIjoiUk9MRV9NQVRFIiwiaWF0IjoxNzU1OTkyODY0LCJleHAiOjE3NTU5OTY0NjR9.SHfXK_1BLwNQuFJGQ8yGbDzTi0jcsK-T8LjPmqFIDrc`
     }
 
     return config
--- src/features/onboarding/entry/OnBoardingOwnerPage.jsx
+++ src/features/onboarding/entry/OnBoardingOwnerPage.jsx
@@ -1,3 +1,53 @@
+import { useState } from 'react'
+import './Onboarding.css'
+import { Icon } from '../../../components/Icon/Icon'
+import { useNavigate } from 'react-router-dom'
+
 export const OnBoardingOwnerPage = () => {
-  return <h1>자영업자 온보딩입니다.</h1>
+  const [step, setStep] = useState(0)
+  const navigate = useNavigate()
+
+  const pages = [
+    {
+      name: 'onboard-owner-1',
+      button: '다음',
+    },
+    {
+      name: 'onboard-owner-2',
+      button: '다음',
+    },
+    {
+      name: 'onboard-owner-3',
+      button: '시작하기',
+    },
+  ]
+
+  const handleNext = () => {
+    if (step < pages.length - 1) {
+      setStep(step + 1)
+    } else {
+      alert('온보딩이 완료되었습니다!') // 추후 라우팅 연결 가능
+      navigate('/home/owner')
+    }
+  }
+
+  return (
+    <div className='onboarding'>
+      <div className='onboarding-content'>
+        <Icon name={pages[step].name} width={360} height={400} />
+      </div>
+
+      {/* indicator */}
+      <div className='indicator'>
+        {pages.map((_, idx) => (
+          <span key={idx} className={idx === step ? 'dot active' : 'dot'} />
+        ))}
+      </div>
+
+      {/* button */}
+      <button className='next-btn' onClick={handleNext}>
+        {pages[step].button}
+      </button>
+    </div>
+  )
 }
