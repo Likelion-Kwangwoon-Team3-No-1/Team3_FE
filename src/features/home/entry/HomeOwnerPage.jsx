320444d1966a37f86ba0c513d64a6225b03c2f93
Eine0322 <eine113589@gmail.com>
2025-08-25 17:15:49 +0900

    [feat] sns 링크 재연결 (feedup.official)

--- src/features/home/entry/HomeOwnerPage.jsx
+++ src/features/home/entry/HomeOwnerPage.jsx
@@ -126,7 +126,7 @@ export const HomeOwnerPage = () => {
           <Carousel promotions={promotions} />
 
           <div className='sns-btn'>
-            <button onClick={() => window.open('https://www.instagram.com/instagram/')}>
+            <button onClick={() => window.open('https://www.instagram.com/feedup.official/')}>
               <Icon name='logo-instagram' width={32} height={32} className='sns-btn-logo' />
               <span className='sns-btn-content'>Feed Up SNS 바로가기</span>
               <Icon name='button-sns-arrow' width={24} height={24} className='sns-btn-arrow' />
